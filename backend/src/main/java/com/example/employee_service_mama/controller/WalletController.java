34ce827f3fad814cfc6000d32d5b4b4439aa0699
srinivasulu2004 <srinivasulubehara@gmail.com>
2026-02-20 18:08:38 +0530

    Update WalletController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/WalletController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/WalletController.java
@@ -17,8 +17,8 @@ import java.util.List;
         origins = {
                 "https://teamhub.in",
                 "http://teamhub.in",
-                "http://teamhub-ALB-1584591762.ap-south-1.elb.amazonaws.com",
-                "http://65.2.144.168:80"
+                "http://teamhub-ALB-680655485.ap-south-1.elb.amazonaws.com",
+                "http://15.206.26.130:80"
         },
         allowCredentials = "true"
 )
@@ -100,4 +100,5 @@ public class WalletController {
     }
 
 }
+
 
