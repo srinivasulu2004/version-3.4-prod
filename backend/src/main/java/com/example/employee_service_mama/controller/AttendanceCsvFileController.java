3263d8b07a3b6ca27783f3fe94432c217ee9549d
srinivasulu2004 <srinivasulubehara@gmail.com>
2026-02-20 18:01:03 +0530

    Update AttendanceCsvFileController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/AttendanceCsvFileController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/AttendanceCsvFileController.java
@@ -17,8 +17,8 @@ import java.util.Map;
         origins = {
                 "https://teamhub.in",
                 "http://teamhub.in",
-                "http://teamhub-ALB-1584591762.ap-south-1.elb.amazonaws.com",
-                "http://65.2.144.168:80"
+                "http://teamhub-ALB-680655485.ap-south-1.elb.amazonaws.com",
+                "http://15.206.26.130:80"
         },
         allowCredentials = "true"
 )public class AttendanceCsvFileController {
@@ -41,5 +41,6 @@ import java.util.Map;
     }
 
 }
+
 
 
