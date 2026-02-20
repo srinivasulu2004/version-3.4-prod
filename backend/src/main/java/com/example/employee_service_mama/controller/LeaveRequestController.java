7bbe47df863305ce9c82aa8342b9c60c9a6e931f
srinivasulu2004 <srinivasulubehara@gmail.com>
2026-02-20 16:07:26 +0530

    Update LeaveRequestController.java

--- backend/src/main/java/com/example/employee_service_mama/controller/LeaveRequestController.java
+++ backend/src/main/java/com/example/employee_service_mama/controller/LeaveRequestController.java
@@ -14,9 +14,8 @@ import java.util.Map;
         origins = {
                 "https://teamhub.in",
                 "http://teamhub.in",
-                "http://52.202.113.154:80",
-                "http://127.0.0.1:5173",
-                "http://localhost:5173"
+                "http://teamhub-ALB-1584591762.ap-south-1.elb.amazonaws.com",
+                "http://65.2.144.168:80",
         },
         allowCredentials = "true"
 )
@@ -80,3 +79,4 @@ public class LeaveRequestController {
     }
 
 }
+
